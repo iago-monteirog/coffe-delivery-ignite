891f4a2f20dad913f2b8687cc397ce9419503ebf
iago-monteirog <iagomgarcia9000@gmail.com>
2023-06-14 19:33:20 -0300

    feat: add global style and theme provider

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+    white: '#FFFFFF',
+    
+    background: '#FAFAFA',
+    baseCard: '#F3F2F2',
+    baseInput: '#EDEDED',
+    baseButton: '#E6E5E5',
+    baseHover: '#E6E5E5',
+    baseLabel: '#8D8686',
+    baseText: '#574F4D',
+    baseSubtitle: '#403937',
+    baseTitle: '#272221',
+
+    yellowDark: '#C47F17',
+    yellow: '#DBAC2C',
+    yellowLight: '#F1E9C9',
+
+    purpleDark: '#4B2995',
+    purple: '#8047F8',
+    purpleLight: '#EBE5F9',
+}
